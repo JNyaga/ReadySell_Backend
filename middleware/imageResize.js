bc7ef0b321149ed473ae661483c7d1c40fdd016e
Muriithi Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-12-31 13:07:04 +0300

    using the tmp

--- middleware/imageResize.js
+++ middleware/imageResize.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const cloudinary = require('cloudinary').v2;
 
 
-const outputFolder = "public/assets";
+const outputFolder = "/tmp/" //"public/assets";
 
 module.exports = async (req, res, next) => {
   const images = [];
@@ -14,22 +14,22 @@ module.exports = async (req, res, next) => {
       .resize(2000)
       .jpeg({ quality: 50 })
       .toFile(path.resolve(outputFolder, file.filename + "_full.jpg"));
-    
-    await cloudinary.uploader.upload(path.resolve(outputFolder, file.filename + "_full.jpg"),{ 
+
+    await cloudinary.uploader.upload(path.resolve(outputFolder, file.filename + "_full.jpg"), {
       folder: "readysell/assets/",
-      public_id:file.filename + "_full",
-      overwrite:true
+      public_id: file.filename + "_full",
+      overwrite: true
     })
 
     await sharp(file.path)
       .resize(100)
       .jpeg({ quality: 30 })
       .toFile(path.resolve(outputFolder, file.filename + "_thumb.jpg"));
-    
-    await cloudinary.uploader.upload(path.resolve(outputFolder, file.filename + "_thumb.jpg"),{ 
+
+    await cloudinary.uploader.upload(path.resolve(outputFolder, file.filename + "_thumb.jpg"), {
       folder: "readysell/assets/",
-      public_id:file.filename + "_thumb",
-      overwrite:true
+      public_id: file.filename + "_thumb",
+      overwrite: true
     })
 
     fs.unlinkSync(file.path);
